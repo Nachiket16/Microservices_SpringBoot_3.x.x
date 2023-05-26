2b6df5932de293204cb07a608a5dab11868ad8a0
nachiket.khule <nachiketkhule.nk@gmail.com>
2023-05-26 10:37:54 +0530

    wip

--- UserService/src/main/java/com/nachiket/user/service/UserService/controller/UserController.java
+++ UserService/src/main/java/com/nachiket/user/service/UserService/controller/UserController.java
@@ -2,7 +2,9 @@ package com.nachiket.user.service.UserService.controller;
 
 import com.nachiket.user.service.UserService.entities.User;
 import com.nachiket.user.service.UserService.services.impl.UserServiceImpl;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,10 +14,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/users")
+@Slf4j
 public class UserController {
 
   @Autowired
@@ -23,21 +25,33 @@ public class UserController {
 
 
   @PostMapping
-  public ResponseEntity<User> createUser(@RequestBody User user){
+  public ResponseEntity<User> createUser(@RequestBody User user) {
     User user1 = userService.saveUser(user);
     return ResponseEntity.status(HttpStatus.CREATED).body(user1);
   }
 
+  @GetMapping
+  public ResponseEntity<List<User>> getAllUSer() {
+    List<User> allUser = userService.getAllUser();
+    return ResponseEntity.ok(allUser);
+  }
+
   @GetMapping("/{userId}")
-  public ResponseEntity<User> getSingleUser(@PathVariable String userId){
+  @CircuitBreaker(name = "ratingHotelBreaker", fallbackMethod = "ratingHotelFallback")
+  public ResponseEntity<User> getSingleUser(@PathVariable String userId) {
     User user = userService.getUser(userId);
     return ResponseEntity.ok(user);
   }
 
-  @GetMapping
-  public ResponseEntity<List<User>> getAllUSer(){
-    List<User> allUser = userService.getAllUser();
-    return ResponseEntity.ok(allUser);
+  public ResponseEntity<User> ratingHotelFallback(String userId, Exception ex) {
+//    log.info("Fallback is executed because service is down : ",ex.getMessage());
+    User user = User.builder()
+        .email("dummy@gmail.com")
+        .name("Dummy dum dum")
+        .about("This is created dummy because some service is down")
+        .userId("1234")
+        .build();
+    return new ResponseEntity<>(user, HttpStatus.OK);
   }
 
 }
--- UserService/src/main/java/com/nachiket/user/service/UserService/repository/UserRepository.java
+++ UserService/src/main/java/com/nachiket/user/service/UserService/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.nachiket.user.service.UserService.repository;
 
 import com.nachiket.user.service.UserService.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User,String> {
 
 }
